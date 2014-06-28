9262a260011979206247524835545e166e2b4c78
Hiroki KIYOHARA <hirokiky@gmail.com>
2014-06-29 00:48:10 +0900

    Added child model for mymodel

--- thapp/models.py
+++ thapp/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class MyModel(models.Model):
     name = models.CharField(max_length=255)
+
+
+class Child(models.Model):
+    mymodel = models.ForeignKey(MyModel)
--- thapp/tests.py
+++ thapp/tests.py
@@ -7,3 +7,15 @@ class TestMyModel(TestCase):
         MyModel.objects.create(name='test')
         actual = MyModel.objects.first()
         self.assertEqual(actual.name, 'test')
+
+
+class TestChild(TestCase):
+    def test__mymodel(self):
+        from thapp.models import Child
+        from thapp.models import MyModel
+        mymodel = MyModel.objects.create(name='test')
+        Child.objects.create(mymodel=mymodel)
+
+        actual = Child.objects.first()
+        self.assertEqual(actual.mymodel, mymodel)
+
